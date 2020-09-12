13d41b9e657c848ee38125922ff47201c0afe3ce
Yaroslav Popov <ed.creater@gmail.com>
2020-09-12 16:55:24 +0500

    Work and development page heading fixes

---
+++ frontend/views/article/_sidebar.php
@@ -0,0 +1,7 @@
+<div class="sidebar">
+    <?php echo \common\widgets\SideArticleCategories::widget(); ?>
+
+    <div class="sidebar__widget s-widget">
+        <?php echo \common\widgets\CsSidePosts::widget(['count_items' => 4]); ?>
+    </div>
+</div>
