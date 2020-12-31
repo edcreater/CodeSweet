a2f410fb3eea56887a28597915f1aacb86a637ec
Yaroslav Popov <ed.creater@gmail.com>
2020-12-31 14:03:36 +0500

    Update Sitemap

--- frontend/models/Sitemap.php
+++ frontend/models/Sitemap.php
@@ -14,7 +14,32 @@ class Sitemap extends Model
     public function getUrl()
     {
 
-        $urls = [];
+        $urls = [
+            [
+                'loc' => '/',
+                'lastmod'    => date(DATE_W3C),
+                'changefreq' => 'daily',
+                'priority'   => 1.0
+            ],
+            [
+                'loc' => '/portfolio/',
+                'lastmod'    => date(DATE_W3C),
+                'changefreq' => 'daily',
+                'priority'   => 1.0
+            ],
+            [
+                'loc' => '/blog/',
+                'lastmod'    => date(DATE_W3C),
+                'changefreq' => 'daily',
+                'priority'   => 1.0
+            ],
+            [
+                'loc' => '/developments/',
+                'lastmod'    => date(DATE_W3C),
+                'changefreq' => 'daily',
+                'priority'   => 1.0
+            ],
+        ];
 
         // Articles
         $query = new Query;
@@ -50,6 +75,51 @@ class Sitemap extends Model
                 'priority'   => 1.0
             ];
         }
+
+        // Developments
+        $query = new Query;
+        $query->select([
+            'development.slug AS slug', 'development.updated_at AS updated_at', 'development_category.slug as category_slug'
+        ])
+            ->from('development')
+            ->leftJoin('development_category', 'development_category.id = development.category_id');
+
+        $command = $query->createCommand();
+        $data    = $command->queryAll();
+
+        foreach ($data as $row) {
+            $urls[] = [
+                'loc'        => Yii::$app->urlManager->createUrl([
+                    'development/view', 'category' => $row['category_slug'], 'slug' => $row['slug']
+                ]),
+                'lastmod'    => date(DATE_W3C, $row['updated_at']),
+                'changefreq' => 'daily',
+                'priority'   => 1.0
+            ];
+        }
+
+        // Portfolio
+        $query = new Query;
+        $query->select([
+            'work.slug AS slug', 'work.updated_at AS updated_at', 'work_category.slug as category_slug'
+        ])
+            ->from('work')
+            ->leftJoin('work_category', 'work_category.id = work.category_id');
+
+        $command = $query->createCommand();
+        $data    = $command->queryAll();
+
+        foreach ($data as $row) {
+            $urls[] = [
+                'loc'        => Yii::$app->urlManager->createUrl([
+                    'work/view', 'category' => $row['category_slug'], 'slug' => $row['slug']
+                ]),
+                'lastmod'    => date(DATE_W3C, $row['updated_at']),
+                'changefreq' => 'daily',
+                'priority'   => 1.0
+            ];
+        }
+
 
         return $urls;
     }
--- frontend/views/site/sitemap.php
+++ frontend/views/site/sitemap.php
@@ -6,7 +6,7 @@
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
 
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
     <?php foreach($urls as $url): ?>
         <url>
             <loc><?= $host . $url['loc']; ?></loc>
